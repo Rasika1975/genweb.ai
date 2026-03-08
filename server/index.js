81dbf7334c6c08c610e667db4fe5c19407303d2c
@Rasika1975 <rasikagudulkar1008@gmail.com>
2026-03-08 13:22:19 +0530

    Update CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 8000;
 app.use(express.json());//json data ko read karne ke liye ye ek middelware hai jo request body me json data ko parse karta hai aur usse req.body me store karta hai
 app.use(cookieParser());//ye middleware client se aane wale cookies ko parse karta hai aur unhe req.cookies me store karta hai
 app.use(cors({
-  origin: 'http://localhost:5173',//ye use hota hai kuiki apna sever kisi frontend se request accept karega uska url dena hota hai
+  origin: 'https://genweb-ai-1-cdrc.onrender.com',//ye use hota hai kuiki apna sever kisi frontend se request accept karega uska url dena hota hai
   credentials: true,// token not found error aata hai to isko true karna hota hai
 }))
 app.use("/api/auth",authRouter)
